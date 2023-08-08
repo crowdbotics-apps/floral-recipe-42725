d6421c42db566347144e8ab618d3a8d900457871
Crowdbotics <team@crowdbotics.com>
2023-08-08 13:29:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-recipe-42725",
+  url: "https://floral-recipe-42725.botics.co",
+  api: "https://floral-recipe-42725.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
